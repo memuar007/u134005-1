713408f14844610a58fc54eacc3fc26e99f4fb5e
Alexandr Eremin <memuar007@yandex.ru>
2018-05-26 22:48:38 +0300

    DAO KPI employee has been created

---
+++ local/modules/miet.kpi/lang/en/lib/kpiemployee.php
@@ -0,0 +1,4 @@
+<? $MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "Indicator value";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Date of the value of the indicator";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Date of last change of indicator value"; ?>

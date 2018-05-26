8387bc2efb62be070a6166d3637b4ce835fba441
Alexandr Eremin <memuar007@yandex.ru>
2018-05-26 23:13:53 +0300

    task_9_kpi_manager

--- local/modules/miet.kpi/lang/en/lib/kpiemployee.php
+++ local/modules/miet.kpi/lang/en/lib/kpiemployee.php
@@ -1,4 +1,7 @@
-<? $MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
 $MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "Indicator value";
 $MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Date of the value of the indicator";
-$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Date of last change of indicator value"; ?>
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Date of last change of indicator value";
+$MESS["KPI_ENTITY_UF_PERIOD_FIELD"] = "Reporting period";
+?>
